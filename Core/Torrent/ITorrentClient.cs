58eec30b8703eb741c95bbb01b1755c8d20fcc4b
Thomas Rene Andresen <thomas.r.andresen@gmail.com>
2013-10-22 21:37:40 +0200

    Fixed breaking build

--- Core/Torrent/ITorrentClient.cs
+++ Core/Torrent/ITorrentClient.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Core.Torrent;
 
 namespace CleanShave.Core.Torrent
 {
--- Core/Torrent/TorrentData.cs
+++ Core/Torrent/TorrentData.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using Core.Torrent;
 
 namespace CleanShave.Core.Torrent
 {
